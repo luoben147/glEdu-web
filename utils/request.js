75cea9146af7e862beaf5547c9d6f72e11629daf
luoben_147 <315523376@qq.com>
2020-05-18 14:00:07 +0800

    修改请求地址

--- utils/request.js
+++ utils/request.js
@@ -4,7 +4,8 @@ import cookie from 'js-cookie'
 
 // 创建axios实例
 const service = axios.create({
-    baseURL: 'http://localhost:9001', // api 的 base_url (nginx地址代理)
+    //baseURL: 'http://localhost:9001', // api 的 base_url (nginx地址代理)
+    baseURL: 'http://localhost:8222', // api 的 base_url (网关地址)
     timeout: 20000 // 请求超时时间
 })
 
